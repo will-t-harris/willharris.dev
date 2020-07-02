f9e64d9969d31f5e9dbb2aa25af6eb6f5ba7e7d0
Will Harris <will.t.harris@protonmail.com>
2020-07-02 00:06:27 -0700

    config: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    textColor: {
+      headerText: "#0F1B61",
+    },
+    backgroundColor: {
+      lightModeBody: "#f4f9fc",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
