ab913aa149c64167ac9aeeaec91a6f3196cf52e3
Will Harris <will.t.harris@protonmail.com>
2020-07-02 00:07:42 -0700

    feat: initial work on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,36 @@
+import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import tw, { css } from "twin.macro"
+
+import { ChainIcon } from "../components/icons/chainIcon"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <header tw="bg-lightModeBody">
+    <nav
+      tw="flex mx-auto"
+      css={[
+        css`
+          width: 900px;
+        `,
+      ]}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      <Link to="/">
+        <ChainIcon />
+      </Link>
+      <div
+        tw="flex w-full justify-end self-center text-2xl text-headerText"
+        css={[
+          css`
+            font-family: roboto slab;
+          `,
+        ]}
+      >
+        <Link>Garden</Link>
+        <Link>Bikes</Link>
+        <Link>Projects</Link>
+      </div>
+    </nav>
   </header>
 )
 
