a8be9ec761f26271a2afff97059509cafd3714ac
Will Harris <will.t.harris@protonmail.com>
2020-07-02 23:46:45 -0700

    feat: create garden posts in node

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,10 +5,44 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
   if (node.internal.type === "Mdx") {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
+
     createNodeField({
       node,
       name: "slug",
       value: slug,
     })
   }
+}
+
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
+  const { data } = await graphql(`
+    query nodeQuery {
+      allMdx {
+        nodes {
+          frontmatter {
+            path
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  const postTemplate = require.resolve("./src/templates/postTemplate.js")
+
+  if (data.error) {
+    console.error(`DANGER DANGER, something went wrong`)
+  }
+
+  data.allMdx.nodes.forEach((node) => {
+    if (node.fields.slug) {
+      createPage({
+        path: `/garden${node.frontmatter.path}`,
+        component: postTemplate,
+        context: { slug: node.frontmatter.path },
+      })
+    }
+  })
 }
