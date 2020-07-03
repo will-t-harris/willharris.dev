6eaf3e6f51da766fb8061885c16c04bb81e1bf32
Will Harris <will.t.harris@protonmail.com>
2020-07-02 23:44:22 -0700

    feat: add simple garden index

---
+++ src/pages/garden.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import tw, { css } from "twin.macro"
+
+const Garden = () => {
+  const data = useStaticQuery(graphql`
+    query gardenQuery {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            path
+            garden
+          }
+          id
+        }
+      }
+    }
+  `)
+
+  const { nodes } = data.allMdx
+  return (
+    <div
+      tw="flex flex-col mx-auto text-lightModeText"
+      css={[
+        css`
+          width: 900px;
+          font-family: Inter;
+        `,
+      ]}
+    >
+      {nodes.map((node) => {
+        if (Boolean(node.frontmatter.garden)) {
+          return (
+            <Link to={`/garden${node.frontmatter.path}`} key={node.id}>
+              {node.frontmatter.title}
+            </Link>
+          )
+        }
+      })}
+    </div>
+  )
+}
+
+export default Garden
