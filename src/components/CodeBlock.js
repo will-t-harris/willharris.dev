585e5a8e8e33e680a28fe4315a634794ab0bb0a4
Will Harris <will.t.harris@protonmail.com>
2020-07-06 16:44:52 -0700

    style: remove flex

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -9,7 +9,7 @@ export const CodeBlock = ({ children, className }) => {
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={{ ...style, padding: "20px" }}>
           {tokens.map((line, index) => (
-            <div tw="flex" key={index} {...getLineProps({ line, key: index })}>
+            <div key={index} {...getLineProps({ line, key: index })}>
               {line.map((token, key) => (
                 <span key={key} {...getTokenProps({ token, key })} />
               ))}
