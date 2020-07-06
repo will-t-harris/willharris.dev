d2d5cc8c5469ad89187fa3ebc72fe361691540d6
Will Harris <will.t.harris@protonmail.com>
2020-07-06 16:44:02 -0700

    feat: add basic bikes index page

--- src/pages/bikes.js
+++ src/pages/bikes.js
@@ -1,7 +1,43 @@
 import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import tw, { css } from "twin.macro"
 
 const Bikes = () => {
-  return <div>Bikes!</div>
+  const data = useStaticQuery(graphql`
+    query bikeQuery {
+      allMdx(filter: { frontmatter: { contentCategory: { eq: "bike" } } }) {
+        nodes {
+          frontmatter {
+            title
+            path
+            garden
+            contentCategory
+          }
+          id
+        }
+      }
+    }
+  `)
+
+  const { nodes } = data.allMdx
+  return (
+    <section
+      tw="flex flex-col mx-auto"
+      css={[
+        css`
+          width: 900px;
+          font-family: Inter;
+        `,
+      ]}
+    >
+      <h1 tw="text-4xl mt-16 mb-10 font-bold">My (Current) Bike Herd</h1>
+      {nodes.map((node) => (
+        <Link to={`/bike${node.frontmatter.path}`}>
+          {node.frontmatter.title}
+        </Link>
+      ))}
+    </section>
+  )
 }
 
 export default Bikes
