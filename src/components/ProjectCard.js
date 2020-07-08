402fc23f48d62f398b21dda5c3fb9a040b4f0cbf
Will Harris <will.t.harris@protonmail.com>
2020-07-07 22:45:37 -0700

    feat: add ProjectCard component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { Link } from "gatsby"
+import tw, { css } from "twin.macro"
+
+import { CardButton } from "./CardButton"
+
+export const ProjectCard = ({
+  projectName,
+  sourceUrl,
+  npmUrl,
+  liveUrl,
+  pageUrl,
+}) => {
+  return (
+    <section
+      tw="my-16 shadow-lg"
+      css={[
+        css`
+          height: 28.125rem;
+          width: 56.25rem;
+        `,
+      ]}
+    >
+      <div
+        tw="flex bg-gray-400 rounded-t"
+        css={[
+          css`
+            height: 20rem;
+          `,
+        ]}
+      >
+        <h2 tw="my-auto ml-20 text-2xl font-bold">
+          <Link to={pageUrl}>{projectName}</Link>
+        </h2>
+      </div>
+      <div
+        tw="flex bg-gray-200"
+        css={[
+          css`
+            height: 8.125rem;
+          `,
+        ]}
+      >
+        <CardButton buttonText="Source" buttonUrl={sourceUrl} />
+        {npmUrl && <CardButton buttonText="NPM" buttonUrl={npmUrl} />}
+        {liveUrl && <CardButton buttonText="Live" buttonUrl={liveUrl} />}
+      </div>
+    </section>
+  )
+}
