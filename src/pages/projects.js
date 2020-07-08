010e983e6407eba2b7e01baa17c5bd4096be5fdd
Will Harris <will.t.harris@protonmail.com>
2020-07-07 22:45:50 -0700

    feat: add basic projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,38 @@
 import React from "react"
+import tw, { css } from "twin.macro"
+
+import { ProjectCard } from "../components/ProjectCard"
 
 const Projects = () => {
-  return <div>Projects!</div>
+  return (
+    <section
+      tw="flex flex-col mx-auto"
+      css={[
+        css`
+          width: 900px;
+          font-family: Inter;
+        `,
+      ]}
+    >
+      <h1 tw="text-4xl mt-16 mb-10 font-bold">Projects</h1>
+      <ProjectCard
+        projectName="use-instagram-feed"
+        pageUrl="/projects/use-instagram-feed"
+        sourceUrl="https://github.com/will-t-harris/use-instagram-feed"
+        npmUrl="https://www.npmjs.com/package/use-instagram-feed"
+      />
+      <ProjectCard
+        projectName="move-with-madeline"
+        sourceUrl="https://github.com/will-t-harris/move-with-madeline"
+        liveUrl="https://movewithmadeline.com"
+      />
+      <ProjectCard
+        projectName="abq-bike-trails"
+        sourceUrl="https://github.com/abq-outdoor-trails/outdoor-trails-master"
+        liveUrl="http://abqtrails.deepdivecoding.com/"
+      />
+    </section>
+  )
 }
 
 export default Projects
