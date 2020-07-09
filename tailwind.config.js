d2207c12328ad31f60dd3b6c6ac98d97b341350f
Will Harris <will.t.harris@protonmail.com>
2020-07-08 17:38:37 -0700

    config: add colors to tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
     extend: {
       textColor: {
         lightModeText: "#323d79",
+        darkModeText: "#f4f9fc",
       },
       backgroundColor: {
         lightModeBody: "#f4f9fc",
+        lightModeBlue: "#323d79",
+        darkModeBody: "#272525",
       },
     },
   },
