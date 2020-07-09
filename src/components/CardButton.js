1ececbb26a32a8be4f55cb4e6f11686e2b194f88
Will Harris <will.t.harris@protonmail.com>
2020-07-08 17:38:16 -0700

    style: change button based on dark/light mode

--- src/components/CardButton.js
+++ src/components/CardButton.js
@@ -1,10 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import tw, { css } from "twin.macro"
+import { useTheme } from "../ThemeContext"
 
 export const CardButton = ({ buttonText, buttonUrl }) => {
+  const themeState = useTheme()
   return (
-    <a tw="bg-blue-600 ml-4 p-2 self-center rounded" href={buttonUrl}>
+    <a
+      css={[
+        themeState.dark
+          ? tw`bg-pink-400 text-lightModeText hover:bg-lightModeBlue hover:text-darkModeText`
+          : tw`bg-lightModeBlue text-darkModeText hover:bg-pink-400 hover:text-lightModeText`,
+        tw`ml-4 p-2 self-center rounded text-xs font-bold tracking-wide`,
+      ]}
+      href={buttonUrl}
+    >
       {buttonText}
     </a>
   )
