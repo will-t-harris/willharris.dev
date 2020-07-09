244e35cad6548fd417baf063a0e26df6b2215b25
Will Harris <will.t.harris@protonmail.com>
2020-07-08 17:38:02 -0700

    style: improve project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 import tw, { css } from "twin.macro"
 
 import { CardButton } from "./CardButton"
 
 export const ProjectCard = ({
   projectName,
+  projectImage,
   sourceUrl,
   npmUrl,
   liveUrl,
@@ -23,28 +25,33 @@ export const ProjectCard = ({
       ]}
     >
       <div
-        tw="flex bg-gray-400 rounded-t"
+        tw="rounded-t overflow-hidden border border-pink-400 border-b-0"
         css={[
           css`
             height: 20rem;
           `,
         ]}
       >
-        <h2 tw="my-auto ml-20 text-2xl font-bold">
-          <Link to={pageUrl}>{projectName}</Link>
-        </h2>
+        {projectImage && (
+          <Link to={pageUrl}>
+            <Img fixed={projectImage} />
+          </Link>
+        )}
       </div>
       <div
-        tw="flex bg-gray-200"
+        tw="rounded-b flex border border-pink-400"
         css={[
           css`
             height: 8.125rem;
           `,
         ]}
       >
-        <CardButton buttonText="Source" buttonUrl={sourceUrl} />
+        <CardButton buttonText="SOURCE" buttonUrl={sourceUrl} />
         {npmUrl && <CardButton buttonText="NPM" buttonUrl={npmUrl} />}
-        {liveUrl && <CardButton buttonText="Live" buttonUrl={liveUrl} />}
+        {liveUrl && <CardButton buttonText="LIVE" buttonUrl={liveUrl} />}
+        <h2 tw="self-center justify-end ml-20 text-2xl font-bold">
+          <Link to={pageUrl}>{projectName}</Link>
+        </h2>
       </div>
     </section>
   )
