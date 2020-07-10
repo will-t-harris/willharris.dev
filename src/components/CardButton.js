5412cf4b88811364c9228cd18787aceb9a4b8e00
Will Harris <will.t.harris@protonmail.com>
2020-07-10 14:54:51 -0700

    style: add project card button styles

--- src/components/CardButton.js
+++ src/components/CardButton.js
@@ -5,7 +5,9 @@ import tw, { css } from "twin.macro"
 export const CardButton = ({ buttonText, buttonUrl }) => {
   return (
     <a
-      css={[tw`ml-4 p-2 self-center rounded text-xs font-bold tracking-wide`]}
+      css={[
+        tw`ml-4 p-2 self-center rounded text-xs font-bold tracking-wide bg-lightModeBlue text-darkModeText hover:bg-pink-400 hover:text-lightModeText`,
+      ]}
       href={buttonUrl}
     >
       {buttonText}
