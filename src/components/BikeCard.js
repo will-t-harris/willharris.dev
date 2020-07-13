2a8f7f306f5e52996e4c393ce63573f17325e2af
Will Harris <will.t.harris@protonmail.com>
2020-07-12 22:46:31 -0700

    style: tweak bike card

--- src/components/BikeCard.js
+++ src/components/BikeCard.js
@@ -3,17 +3,28 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 import tw from "twin.macro"
 
-export const BikeCard = ({ path, title, image }) => {
+export const BikeCard = ({ path, title, image, keywords }) => {
   return (
-    <>
-      <Link tw="flex border-b self-center w-full" to={path}>
-        <span tw="self-center ml-24 font-semibold text-xl">{title}</span>
+    <div tw="flex flex-col lg:grid lg:grid-cols-2 lg:border-b bg-gray-200">
+      <Link to={path}>
+        <h2 tw="self-center pl-4 py-4 lg:mt-0 font-bold text-xl bg-gray-200">
+          {title}
+        </h2>
+        <div tw="bg-gray-700 p-1">
+          {keywords.map((keyword) => (
+            <span tw="mt-4 ml-2 text-white italic text-xs font-bold tracking-wide">
+              {keyword}
+            </span>
+          ))}
+        </div>
+      </Link>
+      <Link tw="flex border-b lg:border-0 w-full bg-gray-200" to={path}>
         <Img
-          tw="flex ml-auto mr-4 my-4 rounded-lg"
+          tw="mx-auto lg:mr-4 my-4 rounded-lg"
           objectFit="right"
           fixed={image}
         />
       </Link>
-    </>
+    </div>
   )
 }
