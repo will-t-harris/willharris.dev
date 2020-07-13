27a38550a27f4af35ff8ba3fad5c9362a2418888
Will Harris <will.t.harris@protonmail.com>
2020-07-12 21:48:32 -0700

    feat: add filtering input to garden page

--- src/pages/garden.js
+++ src/pages/garden.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import tw, { css } from "twin.macro"
 
 const Garden = () => {
+  const [filter, setFilter] = useState("")
   const data = useStaticQuery(graphql`
     query gardenQuery {
       allMdx(filter: { frontmatter: { contentCategory: { eq: "post" } } }) {
         nodes {
+          __typename
           frontmatter {
             title
             path
@@ -19,7 +21,18 @@ const Garden = () => {
     }
   `)
 
-  const { nodes } = data.allMdx
+  const { nodes: posts } = data.allMdx
+
+  const searchResults = posts.filter((post) => {
+    if (post.__typename === "Mdx" && post.frontmatter.garden === true) {
+      return post.frontmatter.title.toLowerCase().includes(filter.toLowerCase())
+    }
+  })
+
+  const handleSearch = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <section
       tw="flex flex-col mx-auto"
@@ -31,14 +44,28 @@ const Garden = () => {
       ]}
     >
       <h1 tw="text-4xl mt-16 mb-10 font-bold">Digital Garden</h1>
-      {nodes.map((node) => {
-        if (node.frontmatter.garden) {
-          return (
-            <Link to={`/garden${node.frontmatter.path}`} key={node.id}>
-              {node.frontmatter.title}
-            </Link>
-          )
-        }
+      <input
+        tw="py-1 px-3 rounded-md mb-6 text-lg w-full"
+        value={filter}
+        type="text"
+        onChange={handleSearch}
+        placeholder="Looking for something?"
+      />
+      {searchResults.map(({ frontmatter }, index) => {
+        const title = frontmatter.title
+        return (
+          <div
+            className="mb-3 flex items-center  hover:bg-dark-bg p-2 rounded"
+            key={index}
+          >
+            <span className="mr-2"></span>
+            <h4 className="text-base">
+              <Link className="no-gradient" to="/">
+                {title}
+              </Link>
+            </h4>
+          </div>
+        )
       })}
     </section>
   )
