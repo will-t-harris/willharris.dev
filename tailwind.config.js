446ba6db071901fbe2a5ee3a67cfdbd8db0715c8
Will Harris <will.t.harris@protonmail.com>
2020-07-12 21:21:49 -0700

    config: add spacing to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,17 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      textColor: {
-        lightModeText: "#323d79",
-        darkModeText: "#f4f9fc",
-      },
       backgroundColor: {
         lightModeBody: "#f4f9fc",
         lightModeBlue: "#323d79",
         darkModeBody: "#272525",
+      },
+      textColor: {
+        lightModeText: "#323d79",
+        darkModeText: "#f4f9fc",
+      },
+      spacing: {
+        900: "56.25rem",
       },
     },
   },
