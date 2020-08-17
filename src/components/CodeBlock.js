89e4e525be425bfa75a289a4e0c74c8975f5ebcf
Will Harris <will.t.harris@protonmail.com>
2020-08-17 12:36:29 -0700

    feat: add line highlighting to CodeBlock

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,10 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Highlight, { defaultProps } from "prism-react-renderer"
-import tw from "twin.macro"
+import tw, { css } from "twin.macro"
+import rangeParser from "parse-numeric-range"
 
-export const CodeBlock = ({ children, className }) => {
+const calculateLinesToHighlight = (meta) => {
+  const RE = /{([\d,-]+)}/
+  if (RE.test(meta)) {
+    const strlineNumbers = RE.exec(meta)[1]
+    const lineNumbers = rangeParser(strlineNumbers)
+    return (index) => lineNumbers.includes(index + 1)
+  } else {
+    return () => false
+  }
+}
+
+export const CodeBlock = ({ children, className, metastring }) => {
   const language = className.replace(/language-/, "")
+  const shouldHighlightLine = calculateLinesToHighlight(metastring)
   return (
     <div tw="px-4 w-screen lg:w-900">
       <Highlight {...defaultProps} code={children} language={language}>
@@ -14,13 +27,32 @@ export const CodeBlock = ({ children, className }) => {
             className={className}
             style={{ ...style, padding: "20px" }}
           >
-            {tokens.map((line, index) => (
-              <div key={index} {...getLineProps({ line, key: index })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, index) => {
+              const lineProps = getLineProps({
+                line,
+                key: index,
+                css: shouldHighlightLine(index)
+                  ? css`
+                      background-color: rgb(53, 59, 69);
+                      display: block;
+                      margin-right: -1em;
+                      margin-left: -1em;
+                      padding-right: 1em;
+                      padding-left: 0.75em;
+                      border-left: 0.3em solid #f99;
+                    `
+                  : {},
+              })
+
+              return (
+                <div key={index} {...lineProps}>
+                  {line.map((token, key) => {
+                    const tokenProps = getTokenProps({ token, key })
+                    return <span key={key} {...tokenProps} />
+                  })}
+                </div>
+              )
+            })}
           </pre>
         )}
       </Highlight>
