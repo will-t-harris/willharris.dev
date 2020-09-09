a28d0c0eb1ef2af3c5db8145c38db90946a39921
Will Harris <will.t.harris@protonmail.com>
2020-09-08 22:06:42 -0700

    refactor: remove bikes with no post

--- src/pages/bikes.js
+++ src/pages/bikes.js
@@ -11,33 +11,33 @@ const Bikes = ({ path }) => {
       LHT: imageSharp(
         fixed: { originalName: { eq: "long-haul-trucker.jpg" } }
       ) {
-        fixed(width: 160, height: 160) {
+        fixed(width: 400, height: 400) {
           ...GatsbyImageSharpFixed
         }
       }
       trek990: imageSharp(
         fixed: { originalName: { eq: "trek-990-troll-fork.jpg" } }
       ) {
-        fixed(width: 160, height: 160) {
+        fixed(width: 400, height: 400) {
           ...GatsbyImageSharpFixed
         }
       }
       konaUnit: imageSharp(
         fixed: { originalName: { eq: "kona-unit-01.jpg" } }
       ) {
-        fixed(width: 160, height: 160) {
+        fixed(width: 400, height: 400) {
           ...GatsbyImageSharpFixed
         }
       }
       surlyKrampus: imageSharp(fixed: { originalName: { eq: "krampus.jpg" } }) {
-        fixed(width: 160, height: 160) {
+        fixed(width: 400, height: 400) {
           ...GatsbyImageSharpFixed
         }
       }
       surlyPackRat: imageSharp(
         fixed: { originalName: { eq: "surly-pack-rat.jpg" } }
       ) {
-        fixed(width: 160, height: 160) {
+        fixed(width: 400, height: 400) {
           ...GatsbyImageSharpFixed
         }
       }
@@ -47,7 +47,7 @@ const Bikes = ({ path }) => {
   return (
     <>
       <SEO title="Bikes" pathname={path} />
-      <section tw="flex flex-col mx-auto w-64 lg:w-full">
+      <section tw="flex flex-col mx-auto mb-8 w-64 lg:w-full">
         <h1 tw="text-4xl mt-12 mb-10 font-extrabold lg:text-center">
           My (Current) Bike Herd
         </h1>
@@ -57,35 +57,18 @@ const Bikes = ({ path }) => {
             path="/bikes/surly-long-haul-trucker"
             title="2012 Surly Long Haul Trucker"
             image={data.LHT.fixed}
-            keywords={["Touring,", "Steel,", "700c"]}
           />
           <BikeCard
             twClasses={tw`lg:col-start-3`}
             path="/bikes/1995-trek-990"
             title="1995 Trek 990"
             image={data.trek990.fixed}
-            keywords={["MTB", "Rigid", "Steel", '26"']}
           />
           <BikeCard
             twClasses={tw`lg:col-start-2 lg:row-start-2`}
             path="/bikes/2015-kona-unit"
             title="2015 Kona Unit"
             image={data.konaUnit.fixed}
-            keywords={["MTB", "Rigid", "Steel", "SS", '29"']}
-          />
-          <BikeCard
-            twClasses={tw`lg:col-start-3 lg:row-start-2`}
-            path="/bikes/2018-surly-krampus"
-            title="2018 Surly Krampus"
-            image={data.surlyKrampus.fixed}
-            keywords={["MTB", "Rigid", "Steel", '29"', "Plus"]}
-          />
-          <BikeCard
-            twClasses={tw`lg:col-start-3 lg:row-start-3`}
-            path="/bikes/2019-surly-pack-rat"
-            title="2019 Surly Pack Rat"
-            image={data.surlyPackRat.fixed}
-            keywords={["Steel", "Porteur", "650b"]}
           />
         </div>
       </section>
